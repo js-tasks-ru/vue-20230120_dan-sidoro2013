03eb6f2c70694c25647868067151207b55e5e4d4
Sidorov Denis <dan.sidoro2013@yandex.ru>
2023-01-26 23:02:23 +0400

    second task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+        mathOperation : {
+            operation: ""
+        },
+
+        inpValue : {
+            firstInp: 0,
+            secondInp: 0,
+        },
+
+        answer:{
+            value: 0,
+        }
+    };
+  },
+
+  computed: {
+    recalculationValue(){
+        if(this.mathOperation.operation === 'sum'){
+            return this.answer.value = this.inpValue.firstInp + this.inpValue.secondInp
+        }else if(this.mathOperation.operation === 'subtract'){
+            return this.answer.value = this.inpValue.firstInp - this.inpValue.secondInp
+        }else if(this.mathOperation.operation === 'multiply'){
+            return this.answer.value = this.inpValue.firstInp * this.inpValue.secondInp
+        }else{
+            return this.answer.value =  this.inpValue.firstInp / this.inpValue.secondInp
+        }
+    }
+  },
+
+  methods: {
+  },
+});
+const app = createApp(App);
+const vm = app.mount('#app');
+
