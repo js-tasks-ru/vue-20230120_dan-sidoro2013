dfa99e29cedff971b4a21a3dbd32022fc81a3920
Sidorov Denis <dan.sidoro2013@yandex.ru>
2023-01-26 21:37:47 +0400

    first lesson

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,19 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+        counter: 0, 
+    };
+  },
+  methods: {
+    changeCounter(){
+        this.counter += 1
+    }
+  },
+});
+const app = createApp(App);
+const vm = app.mount('#app');
+
